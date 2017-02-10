97b20897929c59c61d2b26e86203d9f4561ca4f0
Beregulya <beregulya.dev@gmail.com>
2017-02-10 19:23:17 +0200

    PersonOverviewController (added)

--- src/dev/beregulya/address/MainApp.java
+++ src/dev/beregulya/address/MainApp.java
@@ -1,18 +1,34 @@
 package dev.beregulya.address;
 
+import dev.beregulya.address.model.Person;
+import dev.beregulya.address.view.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    public MainApp() {
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -41,6 +57,8 @@ public class MainApp extends Application {
             loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
             AnchorPane personOverview = loader.load();
             rootLayout.setCenter(personOverview);
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,6 +66,10 @@ public class MainApp extends Application {
 
     public Stage getPrimaryStage() {
         return primaryStage;
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
     }
 
     public static void main(String[] args) {
